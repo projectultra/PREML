f41de300ab9ef66eac37399b6c44536343044433
tornado-hub <diasdickson93@gmail.com>
2025-04-16 14:25:59 +0530

    changed port to 8000

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,7 +4,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    port:8000,
+    host:'0.0.0.0',
     proxy: {
+      
       '/api': {
         target: process.env.VITE_API_URL || 'http://localhost:5000',
         changeOrigin: true,
